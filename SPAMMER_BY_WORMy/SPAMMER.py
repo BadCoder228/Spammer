c59864e254a6a9917083e0a9db6208dd61ccc350
Wormy <167223943+BadCoder228@users.noreply.github.com>
2025-02-17 20:53:07 +0700

    Update SPAMMER.py

--- SPAMMER_BY_WORMy/SPAMMER.py
+++ SPAMMER_BY_WORMy/SPAMMER.py
@@ -76,9 +76,8 @@ def agreement_and_db_insert(message):
     cure = db.cursor()   
     db.execute('CREATE TABLE IF NOT EXISTS data(un text,pn text,id integer ,st integer, la text, lo text)')
     cure.execute("SELECT * FROM data WHERE id =?", [message.from_user.id])
-        
+    
     if cure.fetchone() is None:
-            
             cure.execute("INSERT INTO data (un,pn,id,st,la,lo)VALUES (?,?,?,?,?,?)", ('@'+message.from_user.username, str(uid_contact.get(str(message.from_user.id))), message.from_user.id, 0, str(message.location.latitude),str(message.location.longitude)))
             db.commit()
     cure.execute("SELECT * FROM data WHERE id =?", [message.from_user.id]);check = cure.fetchone()
@@ -91,12 +90,10 @@ def agreement_and_db_insert(message):
 
 @bot.callback_query_handler(func=lambda call:True)
 def callback(call):
-    
     global main_keyboard
     global cancel
 
     if call.data == 'main_menu':
-
         for dict_ in [phone_number, spam_message_,data_message_,promo_message_,location_message_,after_purchase_,contact_message]:
             try:
                 dict_.pop(call.from_user.id)
@@ -111,11 +108,9 @@ def callback(call):
     elif call.data == 'spam':
         
         if uid_bool.get(call.from_user.id):
-
             spam_message=bot.edit_message_text(chat_id=call.message.chat.id,message_id= call.message.message_id, text='⏳ Please input your phone number below.\n\n🌀 Example: 79236723802',reply_markup=cancel)
             spam_message_.update([(call.from_user.id, spam_message)])
             bot.register_next_step_handler(spam_message,phone_number_check)
-        
         else:
             activate_your_promo(call.message)
     
@@ -125,7 +120,6 @@ def callback(call):
             data_message=bot.edit_message_text(chat_id=call.message.chat.id,message_id= call.message.message_id, text='⏳ Please input your victim\'s username to we may continue.\n\n🌀 Username must look like this: @AxisModel014',reply_markup=cancel)
             data_message_.update([(call.from_user.id,data_message)])
             bot.register_next_step_handler(data_message,data_check)
-        
         else:
             activate_your_promo(call.message)
             
@@ -133,7 +127,6 @@ def callback(call):
         
         try:
             bot.delete_message(message_id=call.message.message_id, chat_id=call.message.chat.id)
-        
         except:
             pass #to keep ur terminal clean :3
             
@@ -150,11 +143,9 @@ def callback(call):
         bot.answer_callback_query(call.id)
         
         if current_promocode is not None:
-            
             promo_message = bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text= '☑️ Insert your promocode below.',reply_markup=cancel)
             promo_message_.update([(call.from_user.id, promo_message)])
             bot.register_next_step_handler(promo_message, promo_input)
-        
         else:
             bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text= '✖️ There\'s no generated promocodes yet.',reply_markup=cancel)
 
@@ -169,6 +160,7 @@ def spam_error(message, error):
 def promo_input(message):
     global current_promocode
     bot.delete_message(message_id=message.message_id,chat_id=message.chat.id)
+    
     if message.text == current_promocode:
         db = sqlite3.connect(f'{path}data.db')
         cure = db.cursor()
@@ -177,7 +169,6 @@ def promo_input(message):
         uid_bool.update([(message.from_user.id, True)])
         current_promocode=None
         bot.edit_message_text(chat_id=message.chat.id, message_id=promo_message_.get(message.from_user.id).message_id, text='☑️ Success! Promocode\'s activated!',reply_markup=ty.InlineKeyboardMarkup(keyboard=[[ty.InlineKeyboardButton(text='📃 To main menu',callback_data='main_menu')]]))
-    
     else:
         bot.edit_message_text(chat_id=message.chat.id, message_id=promo_message_.get(message.from_user.id).message_id, text='✖️ Promocode not found!',reply_markup=cancel)
     promo_message_.pop(message.from_user.id)
@@ -186,28 +177,27 @@ def promo_input(message):
 def phone_number_check(message):
     phone_number.update([(message.from_user.id , message.text)])
     bot.delete_message(message.chat.id,message.message_id)
+    
     try:
+        
         if ph.is_valid_number(ph.parse('+'+str(phone_number.get(message.from_user.id)))) and int(phone_number.get(message.from_user.id)):
             laps = bot.edit_message_text("🏁 Great, now input laps value.\n\n🌀 The value must be between 1 and 1000",message_id=spam_message_.get(message.from_user.id).message_id,chat_id=message.chat.id, reply_markup=cancel)
             bot.register_next_step_handler(laps,laps_check)
-            
         else:
             raise(RuntimeError)
-    
     except:
         spam_error(message, '📲 Incorrect phone number!')
 
 def laps_check(message):
     bot.delete_message(message.chat.id,message.message_id)
     try:
+        
         if int(message.text) >= 1 or int(message.text) <= 1000 and '-' not in message.text:
-            
             with open('config.json') as file:
                 is_running = json.load(file)
             
             if bool(int(is_running.get('atack'))):
                 spam_error(message, '🖍 Try again later, sombody\'s using this function now!')
-            
             else:
                 with open(f'config.json', 'r') as file:
                     data_to_insert=json.load(file)
@@ -219,10 +209,8 @@ def laps_check(message):
                 data_to_insert['atack'] = 0
                 change_data(data_to_insert)
                 spam_message_.pop(message.from_user.id)
-
         else:
             raise(ValueError)
-    
     except ValueError:
         spam_error(message, '🏁 Incorrect laps value!')
 
@@ -240,7 +228,6 @@ def data_check(message):
     data =cure.fetchone()
     
     if data is not None:
-        
         if not os.path.exists(f'{index}{data[2]}.html'):
             map_file = folium.Map(location=[data[0], data[1]], zoom_start=9)
             folium.Marker([data[0],data[1]], popup='Their exatc geo location.').add_to(map_file)
@@ -250,7 +237,6 @@ def data_check(message):
             bot.edit_message_text('✅ Success! You may now return to main menu.',message_id=data_message_.get(message.from_user.id).message_id,chat_id=message.chat.id, reply_markup=ty.InlineKeyboardMarkup(keyboard=[[ty.InlineKeyboardButton(text='📃 To main menu',callback_data='main_menu')]]))
             file_=bot.send_document(chat_id=message.chat.id,document=send,caption=f'☑️ User found! Their phone number:{data[2]}', reply_markup=ty.InlineKeyboardMarkup(keyboard=[[ty.InlineKeyboardButton(text='🗑 Delete message (until you go to main menu)',callback_data='del_data')]]))
             send.close()
-    
     else:
         bot.edit_message_text('👀 Looks like this user didn\'t use this bot before.\n\n🌀 Data not found.',message_id=data_message_.get(message.from_user.id).message_id,chat_id=message.chat.id, reply_markup=cancel)
     
